74431edf0526cdd40b272018efed557a4549a581
adrianAraqueG <as>
2023-10-05 12:34:06 -0500

    feat: :wrench: Se definió y agregó la conexión a la base de datos en Program.cs (ARCHIVO PRINCIPAL)
    Se agregaron las configuraciones de la base de datos a la instancia principal del proyecto.

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+/*
+* CONFIGURACIÓN PREVIA
+*
+* Cuando creamos 'builder' estamos creando una instancia de configuración para nuestro proyecto
+* de .NET a la que iremos añadiendo servicios tanto como requiera nuestro proyecto.
+*
+*Por ejemplo:
+* Estamos agregando nuestro DbContext que previamente hemos definido en nuestra carpeta
+* de persistencia. En la sentencia de este servicio estamos referenciando el string de conexión
+* previamente definido en 'appsettings.json' y agregando una propiedad que configura automáticamente
+* la versión del engine de MySQL 'ServerVersion.AutoDetect(connectionString)
+*/
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<APIContext>(options =>{
+    string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+
+/*
+* INICIO DE APLICACIÓN
+*/
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
