6f8f5dc936f0e2ec2f915c7c073f17932f7d7046
adrianAraqueG <as>
2023-10-08 13:06:33 -0500

    feat: :art: Se mejoró la estructura de los endpoints de User
    Se crearon dos métodos nuevos para servir distinta información de User; una básica y otra detallada.

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,21 +1,35 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
-using Domain.Entities;
+using API.Dtos;
 using Domain.Interfaces;
+using AutoMapper;
 
 namespace API.Controllers;
 
 public class UserController : ApiBaseController
 {
     public readonly IUnitOfWork _unitOfWork;
-    public UserController(IUnitOfWork unitOfWork){
+    public readonly IMapper _mapper;
+    public UserController(IUnitOfWork unitOfWork, IMapper mapper){
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
-    [HttpGet("all-users")]
-    public async Task<ActionResult> GetAll(){
+    [HttpGet("list/basic")]
+    public async Task<ActionResult> GetUsersBasic(){
         var users = await _unitOfWork.Users.GetAllAsync();
 
-        return Ok(users);
+        var usersMapped = _mapper.Map<IEnumerable<BasicUserDto>>(users);
+        
+        return Ok(usersMapped);
+    }
+
+    [HttpGet("list/all")]
+    public async Task<ActionResult> GetUsersAll(){
+        var users = await _unitOfWork.Users.GetAllUsers();
+
+        var usersMapped = _mapper.Map<IEnumerable<UserDto>>(users);
+        
+        return Ok(usersMapped);
     }
 }
